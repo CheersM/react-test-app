1a0b1eab125b7adf00d8554027d9730b87fe6df3
WIN-6OHIEJTVVGG\User <vdiukarev90@gmail.com>
2021-08-13 15:40:02 +0300

    commit

--- server.js
+++ server.js
@@ -1,39 +1,15 @@
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-
-// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
-const app = express();
-app.use(favicon(__dirname + '/build/db.json'));
-
-//здесь наше приложение отдаёт статику
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-//простой тест сервера
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
 });
 
-//обслуживание html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running');
 });
-app.listen(port);
-
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./public/db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build',
-// });
-
-// const PORT = process.env.PORT || 3001;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(PORT, () => {
-//   console.log('Server is running');
-// });
