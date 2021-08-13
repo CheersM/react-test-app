cf7445b58229c0507c3488d121ca324bdbcdd5db
WIN-6OHIEJTVVGG\User <vdiukarev90@gmail.com>
2021-08-13 14:51:50 +0300

    commit

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
-const express = require('express');
-const path = require('json-server');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
-const server = path.create();
-const router = path.router('./public/db.json');
-
-server.use(router);
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('Server is running');
 });
-
-app.listen(PORT);
-
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./public/db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build',
-// });
-
-// const PORT = process.env.PORT || 3001;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(PORT, () => {
-//   console.log('Server is running');
-// });
