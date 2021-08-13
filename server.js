37959a12cdc0c93920f90e903f1ce4e8fa7eed7d
WIN-6OHIEJTVVGG\User <vdiukarev90@gmail.com>
2021-08-13 14:25:25 +0300

    commit

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./public/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('json-server');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
 });
 
-const PORT = process.env.PORT || 3001;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, () => {
-  console.log('Server is running');
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.listen(process.env.PORT || 3001);
+
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./public/db.json');
+// const middlewares = jsonServer.defaults({
+//   static: './build',
+// });
+
+// const PORT = process.env.PORT || 3001;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(PORT, () => {
+//   console.log('Server is running');
+// });
